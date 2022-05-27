5a9c9444f8ed10153957695364a7f89f72fe0d1a
thisisyjin <hnesb970@naver.com>
2022-05-27 20:07:38 +0900

    publish ghpages

--- src/components/LevelInfo.jsx
+++ src/components/LevelInfo.jsx
@@ -18,7 +18,7 @@ const LevelInfoBlock = styled.div`
 `;
 
 const LevelIcons = styled.div`
-  width: 250px;
+  width: 260px;
   height: 150px;
   display: flex;
   flex-wrap: wrap;
@@ -26,9 +26,35 @@ const LevelIcons = styled.div`
   align-items: center;
   margin: 0 auto;
   margin-bottom: 14px;
+  position: relative;
 
   svg {
     width: 40px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+      + .level-detail {
+        display: block;
+      }
+    }
+  }
+
+  .level-detail {
+    position: absolute;
+    display: none;
+    background-color: ${colors.trans[0]};
+    color: #fff;
+    font-size: 14px;
+    padding: 8px 18px;
+    border-radius: 5px;
+
+    .pm-title {
+      font-size: 15px;
+      font-weight: 500;
+      margin-right: 9px;
+    }
   }
 
   .very-bad {
@@ -52,6 +78,7 @@ const LevelIcons = styled.div`
     margin-right: 10px;
     margin-left: 6px;
     text-align: left;
+    font-weight: 500;
   }
 `;
 
@@ -61,12 +88,32 @@ const LevelInfo = () => {
       <h3 className="level-title">미세먼지 Level</h3>
       <LevelIcons>
         <PmLevelICO className="very-bad" />
+        <div className="level-detail">
+          <span className="pm-title">PM 10</span>151~
+          <br />
+          <span className="pm-title">PM 2.5</span>76~
+        </div>
         <span className="desc">매우 나쁨</span>
         <PmLevelICO className="bad" />
+        <div className="level-detail">
+          <span className="pm-title">PM 10</span>81~150
+          <br />
+          <span className="pm-title">PM 2.5</span>36~75
+        </div>
         <span className="desc">나쁨</span>
         <PmLevelICO className="soso" />
+        <div className="level-detail">
+          <span className="pm-title">PM 10</span>31~80
+          <br />
+          <span className="pm-title">PM 2.5</span>16~35
+        </div>
         <span className="desc">보통</span>
         <PmLevelICO className="good" />
+        <div className="level-detail">
+          <span className="pm-title">PM 10</span>0~30
+          <br />
+          <span className="pm-title">PM 2.5</span>0~15
+        </div>
         <span className="desc">좋음</span>
       </LevelIcons>
     </LevelInfoBlock>
