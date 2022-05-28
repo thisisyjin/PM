37a8e7402df6f2785d0b1afcbf4db7b1440cc70e
thisisyjin <hnesb970@naver.com>
2022-05-28 10:29:24 +0900

    gh pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <App />
-  </Router>
-);
+root.render(<App />);
